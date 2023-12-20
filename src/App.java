2892103d564b658fb51643270e788db597d66c0e
Hiago nunes pimentel <derundaclash665@gmail.com>
2023-12-20 13:44:47 -0300

    Finalizado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
+import Entities.Product;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+
+        Set<Product> set = new HashSet<>();
+
+        set.add(new Product("TV" , 900.0));
+        set.add(new Product("NoteBook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+
+        Product prod = new Product ("NoteBook", 1200.0);
+
+
+        System.out.println(set.contains(prod));
+
     }
 }
